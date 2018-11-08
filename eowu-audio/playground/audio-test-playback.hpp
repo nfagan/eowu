041817831d58eac550136aa4a0a453e29ff60af1
nfagan <nick@fagan.org>
2018-11-08 16:33:22 -0500

    begin add audio

---
+++ eowu-audio/playground/audio-test-playback.hpp
@@ -0,0 +1,12 @@
+//
+//  audio-test-playback.hpp
+//  eowu-audio
+//
+//  Created by Nick Fagan on 11/8/18.
+//
+
+#pragma once
+
+namespace audio {
+  int main();
+}
---
+++ eowu-audio/playground/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  eowu-common
+//
+//  Created by Nick Fagan on 11/8/18.
+//
+
+#include "audio-test-playback.hpp"
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  std::cout << "OK" << std::endl;
+  
+  int res = audio::main();
+  
+  return 0;
+}
