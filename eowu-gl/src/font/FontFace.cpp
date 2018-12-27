bbf35a27cc565072ded2c1e88c93b3f1c4def699
nfagan <nick@fagan.org>
2018-12-27 11:20:07 -0500

    change single char spec

--- eowu-gl/src/font/FontFace.cpp
+++ eowu-gl/src/font/FontFace.cpp
@@ -33,9 +33,7 @@ void eowu::FontFace::SetPixelSizes(eowu::FontFace::PixelSizes pixel_sizes) {
 }
 
 const eowu::FontGlyph& eowu::FontFace::GetGlyph(char character) const {
-  std::string single_char(1, 'a');
-  single_char[0] = character;
-  
+  std::string single_char(1, character);
   return GetGlyph(single_char);
 }
 
@@ -54,9 +52,7 @@ bool eowu::FontFace::HasGlyph(const std::string &character) const {
 }
 
 bool eowu::FontFace::HasGlyph(char character) const {
-  std::string single_char(1, 'a');
-  single_char[0] = character;
-  
+  std::string single_char(1, character);  
   return HasGlyph(single_char);
 }
 
